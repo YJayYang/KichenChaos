a6e4ed47d11f07657fdde489f823b29921a70cb4
JayYang <YuJ.YANG@outlook.com>
2024-09-08 13:53:54 +0800

    重构player代码

---
+++ Assets/Scripts/GameInput.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameInput : MonoBehaviour
+{
+    private PlayerInputActions playerInputActions;
+
+    private void Awake() {
+        playerInputActions = new PlayerInputActions();
+        playerInputActions.Enable();
+    }
+
+    public Vector2 GetMovementVectorNormalized() {
+        Vector2 inputVector = playerInputActions.player.Move.ReadValue<Vector2>();
+        
+        return inputVector.normalized;
+    }
+}
--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -9,48 +9,38 @@ public class Player : MonoBehaviour {
     //[SerializeField] 属性表示这个变量可以在 Unity 编辑器中进行修改。
     [SerializeField] private float moveSpeed = 7f;
 
-    [SerializeField] private float rotateSpeed = 10f;
+    [SerializeField] private float rotateSpeed = 7f;
+
+    [SerializeField] private GameInput gameInput;
+
+    private bool isWalking;
+
     private void Update() {
 
         // 处理玩家输入并计算输入向量
-        Vector2 inputVector = GetInputVector();
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
 
         //控制玩家移动
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y).normalized;
 
 
-        // 如果有移动输入，更新位置并旋转角色
+        // 如果有输入移动，则移动玩家并旋转角色
         if (moveDir != Vector3.zero) {
             MovePlayer(moveDir);
             SmoothRotatePlayer(moveDir);
+            isWalking = true;  // 设置为行走状态
+        } else {
+            isWalking = false; // 没有输入则不在行走
         }
 
-       
 
     }
 
-
-    private Vector2 GetInputVector() {
-        Vector2 inputVector = new(0, 0);
-        if (Input.GetKey(KeyCode.W)) {
-            inputVector.y = +1;
-
-        }
-        if (Input.GetKey(KeyCode.S)) {
-            inputVector.y = -1;
-
-        }
-        if (Input.GetKey(KeyCode.A)) {
-            inputVector.x = -1;
-
-        }
-        if (Input.GetKey(KeyCode.D)) {
-            inputVector.x = +1;
-
-        }
-
-        return inputVector.normalized;
+    public bool IsWalking() {
+        return isWalking;
     }
+
+
 
 
     private void MovePlayer(Vector3 moveDir) {
