597b117f12d74ae8ad37783a19aa248fb7a4805b
JayYang <YuJ.YANG@outlook.com>
2024-09-09 15:44:40 +0800

    clear counter

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -13,25 +13,16 @@ public class Player : MonoBehaviour {
 
     [SerializeField] private GameInput gameInput;
 
+    [SerializeField] private LayerMask counterLayerMask;
+
     private bool isWalking;
+    private Vector3 lastInteractDir;
+    
 
     private void Update() {
+        HandleMovement();
+        HandleInteractions();
 
-        // 处理玩家输入并计算输入向量
-        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
-
-        //控制玩家移动
-        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y).normalized;
-
-
-        // 如果有输入移动，则移动玩家并旋转角色
-        if (moveDir != Vector3.zero) {
-            MovePlayer(moveDir);
-            SmoothRotatePlayer(moveDir);
-            isWalking = true;  // 设置为行走状态
-        } else {
-            isWalking = false; // 没有输入则不在行走
-        }
 
 
     }
@@ -57,7 +48,63 @@ public class Player : MonoBehaviour {
 
     }
 
+    private void HandleMovement() {
+        // 处理玩家输入并计算输入向量
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+        //控制玩家移动
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y).normalized;
+        float moveDistance = moveSpeed * Time.deltaTime;
+        float playerRadius = .7f;
+        float playerHeight = 2f;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDir, moveDistance);
+
+        if (!canMove) {
+            Vector3 moveDriX = new Vector3(moveDir.x, 0, 0);
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDriX, moveDistance);
+
+            if (canMove) {
+                moveDir = moveDriX;
+            } else {
+                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z);
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
+
+                if (canMove) {
+                    moveDir = moveDirZ;
+                } else {
+
+                }
+            }
+        }
+        if (canMove) {
+            if (moveDir != Vector3.zero) {
+                MovePlayer(moveDir);
+                SmoothRotatePlayer(moveDir);
+                isWalking = true;  // 设置为行走状态
+            } else {
+                isWalking = false; // 没有输入则不在行走
+            }
+        }
+
+    }
+
+    private void HandleInteractions() {
+       
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+      
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y).normalized;
+        if (moveDir != Vector3.zero) {
+            lastInteractDir = moveDir;
+        }
+        float interactDistance = 2f;
+        if(Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance, counterLayerMask)) {
+            if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter)) {
+                clearCounter.Interact();
+            }
+        }
+
+    }
 
 
 
-}
+    }
+
